ecc40b7bc174e3c48cece1a6fd365ae5420bee22
jcq15 <mail.shazi@foxmail.com>
2020-10-19 15:18:08 +0800

    xingzuo, chengyujielong, feihualing

--- baoshi.py
+++ baoshi.py
@@ -6,27 +6,100 @@ import math
 import itertools
 import numpy as np
 import re
+from pypinyin import pinyin, Style
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
 
 '''这样记录日志
 logger.info('This is a log info')
 logger.debug('Debugging')
 logger.warning('Warning exists')
 '''
+logging.basicConfig(level=logging.DEBUG, filename='/home/wechat/wechatbot/output.log')
+logger = logging.getLogger(__name__)
+
+# 成语接龙的管理者
+class cyjl_server:
+    data = []               # 成语们，简单粗暴
+
+    def __init__(self):   
+        self.now = ''       # 当前成语
+        self.index = 0      # 第几个词
+        self.scores = {}    # 大家的分数，字典，key为user_id，value是list，第一个是分数，第二个是昵称
+        self.result = []    # 可以接的词
+        if len(cyjl_server.data) == 0:
+            with open("/home/wechat/wechatbot/cyjl.txt", "r") as f:
+                counter = 0
+                for line in f:
+                    content = line.split("\t")
+                    #word = content[0]
+                    #pinyin = content[1].split("'")
+                    #meaning = content[2].replace("\n", "")
+                    cyjl_server.data.append(content[0])
+                    counter += 1
+            logger.info("Init finished! [%d] words." % (counter))
+        self.start()
+
+    # 开始
+    def start(self):
+        while True:
+            word = random.sample(cyjl_server.data, 1)[0]
+            print(word[-1])
+            result = self.get_start_with(word[-1])
+            if result:       # 后继有人
+                self.result = result
+                self.now = word
+                print(self.now)
+                self.index = 1
+                return True
+
+    # 以某个字开头的成语们
+    def get_start_with(self, last_word):
+        result = [word for word in cyjl_server.data if pinyin(word, style=Style.NORMAL)[0][0] in pinyin(last_word, style=Style.NORMAL)[0]]
+        return result
+
+    # 某人发了个开头正确的消息，接受正义的审判吧
+    def judge(self, answer, username, nickname):
+        if answer in self.result:    # 这该死的群友，数理基础竟然如此扎实
+            self.index += 1
+            self.now = answer
+            if username in self.scores:   # 已经有了，+1，没有则增加
+                self.scores[username][0] += 1
+            else:
+                self.scores[username] = [1,nickname]
+            self.result = self.get_start_with(answer[-1])
+            return True
+        return False
+
+    # 游戏结束
+    def end_game(self):
+        # 成绩排序
+        items=self.scores.items()
+        backitems=[v[1] for v in items]      # 分数，昵称
+        backitems.sort()
+
+        reply = '本次共接龙了%s轮，大家的成绩为：\n' % (self.index-1)
+        for backitem in backitems:
+            reply += '%s:%s分\n' % (backitem[1], backitem[0])
+        return reply
 
 class Baoshi:
     def __init__(self):
-        logging.basicConfig(level=logging.DEBUG, filename='/home/wechat/wechatbot/output.log')
-        self.logger = logging.getLogger(__name__)
+        
         self.model = r'frog2.0启动！淦！已经\h点\m分了！你今天学习了吗？'
         self.function = '我是青蛙！我有这些功能，都要@我噢！\n'\
                            '1. 发送“功能”查看功能；\n'\
                            '2. 发送“报时”报时；\n'\
                            '3. 发送“24点”，给你四个数算24点。一定能算出来，算不出来说明你是傻逼；\n'\
                            '4. 发送“48点”，给你四个数算48点。一定能算出来，算不出来说明你是大傻逼；\n'\
-                           '5. 发送“计算 表达式”进行计算，例如“计算 114000+514”'
+                           '5. 发送“计算 表达式”进行计算，例如“计算 114000+514”;\n'\
+                           '6. 发送“天气 城市”进行计算，例如“天气 北京”;\n'\
+                           '7. 发送“今日运势 星座”进行算命，例如“今日运势 狮子座”'
+        self.status = 0           # 0: 无状态, 'cyjl': 在玩成语接龙
 
     # 获取报时内容
-    def __gettext(self):
+    def gettext(self):
         response_text = ''
         status = False
         for c in self.model:
@@ -117,9 +190,37 @@ class Baoshi:
         return operand_stack[0]
      
     def handle(self, data):
+        # AB学习时间！
+        if data['contact'].name == 'AB':
+            return ('要期中考试了还水群呢？？', 'mention')
+
         if data['mentionSelf']:
             text = data['text'].replace('@青蛙 ', '').replace('@青蛙\u2005', '')
+            userid = data['contact'].id
+            usernick = data['contact'].name
 
+
+            if self.status == 'cyjl':
+                if text == '结束成语接龙':             # 结束
+                    reply = self.cyjl.end_game()
+                    self.status = 0
+                    self.cyjl = None
+                    return reply
+                elif text == '成语接龙':               # 成语接龙
+                    return '你是不是沙雕，我们已经在玩成语接龙了！当前是第%s个成语：\n【%s】' % (self.cyjl.index, self.cyjl.now)
+                elif text == '要看答案':
+                    return ', '.join(self.cyjl.result)
+                elif len(text) == 4:                 # 响应所有四字信息
+                    if self.cyjl.judge(text, userid, usernick):             # 还真接上了
+                        reply = '恭喜%s接龙成功！当前是第%s个成语：\n【%s】' % (usernick, self.cyjl.index, self.cyjl.now)
+                        if 0 == len(self.cyjl.result):                 # 后面没法接
+                            reply += '\n这成语没法接，算了，就玩到这吧！\n' + self.cyjl.end_game()
+                            self.status = 0
+                            self.cyjl = None
+                        return reply
+                    else:   # 没接上
+                        return '%s，不对不对！你说的什么玩意，【%s】不是成语！' % (usernick, text)
+                
             if len(text) >= 6 and text[:4] == '修改模板':
                 self.model = text[5:]
                 return '修改成功！现在的模板是：\n' + self.model
@@ -128,7 +229,7 @@ class Baoshi:
                 return self.function
 
             elif text == '报时':
-                return self.__gettext()
+                return self.gettext()
 
             elif len(text) >= 4 and text[:2] == '计算':
                 _, expression = text.split(' ')
@@ -158,3 +259,98 @@ class Baoshi:
                 num_str_list = [str(c) for c in choice]
                 res = ' '.join(num_str_list)
                 return '4 numbers:' + res
+            
+            # 未上线的今日星座运势功能
+            elif len(text) >= 4 and text[:4] == '今日运势':
+                def get_data(ui, xz):
+                    ri = requests.get(url = ui)   # 访问页面
+                    ri.encoding = ri.apparent_encoding # encoding
+                    soupi = BeautifulSoup(ri.text, 'lxml')  # 解析页面
+                    infor1 = soupi.find('div',class_="c_main").find('ul').find_all('li')
+                    infor2 = soupi.find('div',class_="c_cont").find_all('p')
+                    
+                    res = '你是可爱的' + xz + '宝宝！\n'
+                    for i in range(4):
+                        star_c = int(infor1[i].find('em')['style'].split(':')[1].split('p')[0]) // 16
+                        str_tmp = '【' + infor1[i].text[:-1] + '】'  +  star_c * '★' + (5-star_c) * '☆' + '\n'
+                        str_txt = infor2[i].find('span').text + '\n'
+                        res = res + str_tmp + str_txt
+                        
+                    for i in range(4,10):
+                        str_tmp = '【' + infor1[i].find('label').text[:-1] + '】'+ infor1[i].text.split('：')[1] + '\n'
+                        if i == 4: str_tmp = str_tmp + infor2[i].find('span').text + '\n'
+                        res = res + str_tmp
+
+                    return res
+                xz_cn_to_eng = {
+                                '狮子座':'leo',
+                                '金牛座':'taurus',
+                                '白羊座':'aries',
+                                '双子座':'gemini',
+                                '巨蟹座':'cancer',
+                                '处女座':'virgo',
+                                '天秤座':'libra',
+                                '天蝎座':'scorpio',
+                                '射手座':'sagittarius',
+                                '摩羯座':'capricorn',
+                                '水瓶座':'aquarius',
+                                '双鱼座':'pisces'}       
+                _, xz = text.split(' ')
+                xz_eg = xz_cn_to_eng[xz]
+                url = 'https://www.xzw.com/fortune/' + xz_eg + '/'
+                res = get_data(url, xz)
+                #print(res)
+                return res
+
+            elif len(text) >= 4 and text[:5] == '晚安小青蛙':
+                return '你是猪吗？还睡！别@我了，再@自杀！'
+
+
+            elif len(text) >= 4 and text[:2] == '天气':
+                _, city = text.split(' ')
+                city_id = pd.read_csv('/home/wechat/wechatbot/weather_city_id.csv',encoding='gbk')
+                def get_weather(city):
+                    str_id = str(list(city_id[city_id['城市']==city]['citycode'])[0])
+                    ui = 'http://www.weather.com.cn/weather/' + str_id + '.shtml'
+                    ri = requests.get(url = ui)   # 访问页面
+                    ri.encoding = ri.apparent_encoding # encoding
+
+                    soup = BeautifulSoup(ri.text, 'html.parser')
+                    ul_tag = soup.find('ul', 't clearfix')
+                    body1 = soup.body  # 获取body部分
+                    data = body1.find('div', {'id': '7d'})  # 找到id为7d的div
+                    ul = data.find('ul')  # 获取ul部分
+                    li = ul.find_all('li')  # 获取所有的li
+                    final = city + '7日天气速递：\n'
+
+                    for day in li:   # 对每个li标签中的内容进行遍历
+
+                        data = day.find('h1').string   # 找到日期
+                        temp = '【' + data + '】'
+
+                        inf = day.find_all('p')  # 找到li中的所有p标签
+                        if inf[1].find('span') is None:
+                            temperature_highest = '无'  # 天气当中可能没有最高气温（傍晚）
+                        else:
+                            temperature_highest = inf[1].find('span').string  # 找到最高气温
+                            temperature_highest = temperature_highest
+                        temperature_lowest = inf[1].find('i').string  # 找到最低温
+                        temperature_lowest = temperature_lowest  # 最低温度后面有个℃，去掉这个符号
+
+                        temp += inf[0].string + '   '
+                        temp += '最高:' + str(temperature_highest) + '   '
+                        temp += '最低:' + str(temperature_lowest) + '\n'
+                        final += temp
+                    return final
+                res  = get_weather(city)
+                return res
+
+
+            # 未正式上线的测试功能
+            elif text == '测试图片':
+                return ('/home/wechat/wechatbot/images/testimg.png', 'image')
+
+            elif self.status == 0 and text == '成语接龙':             # 结束
+                self.cyjl = cyjl_server()
+                self.status = 'cyjl'
+                return '成语接龙开始！当前是第%s个成语：\n%s' % (self.cyjl.index, self.cyjl.now)
---
+++ mingyuan.py
@@ -0,0 +1,3 @@
+def handle(data):
+	if data['contact'].name == 'tensorspace':
+		return ('闭嘴吧张量空间，本机器人看不下去了！', 'mention')
